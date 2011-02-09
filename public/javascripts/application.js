0feec2a75c17fd700a35d6a90a6269ef62080e71
saleiva <saleiva@gmail.com>
2011-02-09 13:21:31 +0100

    fixing home styles

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -23,5 +23,12 @@ $(document).ready(function(){
       },200);
   });
 
+  //FUNCTION FOR THE MOSAIC's ELEMENTS
+  $('.mosaic_element_div').hover(function() {
+    $(this).children(".mosaic_label").show();
+   },function(){
+    $(this).children(".mosaic_label").hide();
+  });   
+  
 });
 
