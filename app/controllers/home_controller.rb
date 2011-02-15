8d1255bfb20a249b96f89c46fc7a83682b379767
dcano <dcano@vizzuality.com>
2011-02-15 14:22:19 +0100

    Removed comments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,8 @@ class HomeController < ApplicationController
   before_filter :find_all_features
 
   def show
-
     @random_features = @features.sample(9)
     render :action=>"show",:layout=>"home_layout"
-    #layout "home_layout"
-    #sql="SELECT id from features"
-    
   end
   
 protected
