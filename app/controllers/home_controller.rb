722ed2ba053ac03bbabc9f66a28034d76af1ff6f
dcano <dcano@vizzuality.com>
2011-02-15 14:21:11 +0100

    Randomized home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,21 @@
 class HomeController < ApplicationController
+
+  before_filter :find_all_features
+
   def show
+
+    @random_features = @features.sample(9)
     render :action=>"show",:layout=>"home_layout"
     #layout "home_layout"
     #sql="SELECT id from features"
     
   end
+  
+protected
+
+  def find_all_features
+    @features = Feature.find(:all, :order => "position ASC")
+  end
+  
+  
 end
