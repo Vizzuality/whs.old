fa07518da65773721aead38e0c3065a8c6723035
Fernando Espinosa <fer@ferdev.com>
2011-02-21 11:21:39 +0100

    Fixes a bug with features counter by type

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,7 @@ class StaticController < ApplicationController
   def about
     features            = Feature.scoped
     @whs_total_count    = features.count
-    @whs_cultural_count = features.with_type('cultural').count
-    @whs_natural_count  = features.with_type('natural').count
+    @whs_cultural_count = features.cultural.count
+    @whs_natural_count  = features.natural.count
   end
 end
